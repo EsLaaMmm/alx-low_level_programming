4ba6d1db1080d83bb4b204dbf137c550b0423579
EsLaaMmm <islamcharrik400@gmail.com>
2023-03-23 14:45:13 -0700

    0.main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,31 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-/* more headers goes there */
+#include "main.h"
 
-/* betty style doc for function main goes there */
 /**
- * main - Determines if a number is positive, negative or zero
- *
+ * main - function to test if int is pos or neg
  * Return: 0
-*/
-void positive_or_negative(int i)
+ */
+
+int main(void)
 {
+	int i;
 
-	/* your code goes there */
+	i = 0;
+	positive_or_negative(i);
 
-
-	if (i > 0)
-	{
-
-	printf("%d is positive\n", i);
-
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else
-	printf("%d is zero\n", i);
-
+	return (0);
 }
